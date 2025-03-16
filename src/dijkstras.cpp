022ec7a7f52da27b7f190a43b39c2460dcef43ca
katarivn <katarivn@uci.edu>
2025-03-16 18:14:31 +0000

    assign in dijkstra's

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -6,13 +6,11 @@ using namespace std;
 vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous) {
     int n = G.size();
     vector<int> distances(n, INF);
-    previous.resize(n, -1);
-    vector<bool> visited(n, false);
+    previous.assign(n, -1);
 
     auto cmp = [](const pair<int, int>& a, const pair<int, int>& b) {
         return a.second > b.second;
     };
-
     priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(cmp)> pq(cmp);
 
     pq.push({source, 0});
@@ -22,14 +20,11 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
         int u = pq.top().first;
         pq.pop();
 
-        if (visited[u]) continue;
-        visited[u] = true;
-
         for (const Edge& edge : G[u]) {
             int v = edge.dst;
             int weight = edge.weight;
 
-            if (!visited[v] && distances[u] + weight < distances[v]) {
+            if (distances[u] + weight < distances[v]) {
                 distances[v] = distances[u] + weight;
                 previous[v] = u;
                 pq.push({v, distances[v]});
@@ -50,6 +45,7 @@ vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector
     reverse(path.begin(), path.end());
     return path;
 }
+
 
 
 void print_path(const vector<int>& path, int total) {
