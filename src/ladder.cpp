f786d862c1b4495b465b52aacf32535c95e84955
katarivn <katarivn@uci.edu>
2025-03-16 17:37:24 +0000

    whitespace

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -138,7 +138,7 @@ void print_word_ladder(const vector<string>& ladder) {
     cout << "Word ladder found: ";
     for (size_t i = 0; i < ladder.size(); i++) {
         cout << ladder[i];
-        if (i < ladder.size() - 1) {
+        if (i < ladder.size()) { 
             cout << " ";
         }
     }
